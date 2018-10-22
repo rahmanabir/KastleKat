736342907ab70c328c52704375a6e1054e080d9a
Ishfaq <uzushin101@gmail.com>
2018-10-23 04:14:48 +0600

    health mechanics built, castle block class designed, testplayer class created for debuging and traversing game world

---
+++ Assets/Scripts/StoneBlockClass.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class StoneBlockClass : BlockClass {
+
+	private float damageConstant;
+	private int firstcount;
+	//initialization
+	void Start () {
+		originalPos = transform.position;
+		// stoneHealth = 100f;
+		
+		stoneMaxHeatlth = 100f;
+		displacementThreshold = 9f;
+		rb = GetComponent<Rigidbody>();
+		damageConstant = 1.5f;
+		scoreTalier = GameObject.FindGameObjectsWithTag("tallyTool")[0];
+
+		firstcount = 0;
+
+		// tally.setCastleHealth(stoneHealth);
+		// tally.setCastleBlockCount(1);
+
+		// print(tally.getCastleHealth());
+		// print(tally.getCastleBlockCount());
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+		if(firstcount == 0){
+			setHealth(100f);
+			firstcount = 1;
+		}
+
+		destroyLogic();
+
+
+	}
+
+	void OnCollisionEnter(Collision col){
+ 
+     if(col.relativeVelocity.magnitude > 10){ 
+		 
+		float damageAmmount = damageConstant * col.relativeVelocity.magnitude; 
+		 //stoneHealth -= damageAmmount; 
+		deductHealth(damageAmmount);
+		
+		} 
+ 
+ 	}
+
+
+	
+}
---
+++ Assets/Scripts/projectileShooter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileShooter : MonoBehaviour {
+
+	public Rigidbody rockProjectile;
+	public Rigidbody castleBlock;
+	
+	// Update is called once per frame
+	void Update () {
+
+    
+	
+	if(Input.GetButtonDown("Fire1")){
+
+		Rigidbody rock = Instantiate(rockProjectile, transform.position, transform.rotation);
+		rock.velocity = transform.TransformDirection(Vector3.forward * 30);
+	}
+	if(Input.GetButtonDown("Fire2")){
+
+		Vector3 curPos = transform.position;
+		Vector3 normPos = curPos.normalized;
+		Vector3 newPos = curPos+normPos*-5;
+
+		Rigidbody cblock = Instantiate(castleBlock, newPos, transform.rotation);
+
+	}
+
+
+	}
+}
