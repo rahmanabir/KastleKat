cede57212abb9411544f244eeb3a35580f2584d9
Ishfaq <ishfaq.zaman@northsouth.edu>
2018-11-08 23:57:56 +0600

    health metric enabled prefabs added to main branch

--- Assets/Scripts/rockProjectile.cs
+++ Assets/Scripts/rockProjectile.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class rockProjectile : MonoBehaviour {
 
-
 	private float curSpeed = 0; 
 	public float lifetime = 0.2f;
+
+	public int mass = 20;
+	//initialization
+	void Start () {
+
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.mass = mass;
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
