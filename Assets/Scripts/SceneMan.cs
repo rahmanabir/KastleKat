4e1322522e1099c635f8d387d62cb62e60b3f8d6
Jawad Aziz Khan <jawad.aziz.khan.jak@gmail.com>
2018-11-26 18:24:30 +0600

    Pre Merge commit

--- Assets/Scripts/SceneMan.cs
+++ Assets/Scripts/SceneMan.cs
@@ -6,8 +6,8 @@ public class SceneMan : MonoBehaviour {
 	public void LoadCastle() {
         SceneManager.LoadScene("Scene_AR");
     }
-    public void LoadPhysics() {
-        SceneManager.LoadScene("Scene_ARphy");
+    public void LoadNoBox() {
+        SceneManager.LoadScene("Scene_ARnoBox");
     }
     public void LoadMulti() {
         SceneManager.LoadScene("Scene_MP");
